5613bc62fa6774fdaa1d62ab7907052356a98f58
kw4b <102009440+kw4b@users.noreply.github.com>
2023-12-12 17:03:48 -0600

    Add files via upload

---
+++ egg01.py
@@ -0,0 +1,19 @@
+print(" ")
+pet_name = input("What is your pet's name?: ")
+wellness = 100
+energy = 100
+import random
+temp = random.randint(29,41)
+hunger = random.randint(0,100)
+
+print(" ")
+print(pet_name + " is purrfect :3")
+print(" ")
+print("---------------------------")
+print("STATS FOR: "+ pet_name.upper())
+print("---------------------------")
+print("Wellbeing: " + str(wellness))
+print("Energy: " + str(energy))
+print("Temperature: " + str(temp))
+print("Hunger: ")
+print("---------------------------")
